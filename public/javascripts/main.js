e88e74729977861bcb0b80b04afa022a054c05b1
Abhijit Kumar Choudhury <abhi@azureiken.com>
2016-06-27 11:29:41 +0530

    Added File handing examples

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,22 +22,51 @@
             allNavs[index].setAttribute('style', 'text-decoration: underline;');
         }
 
+        //
         //File API
-
-        // var selectedFile = document.getElementById('input').files[0];
+        //
+        //
+        // Check for the various File API support.
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            // Great success! All the File APIs are supported.
+        } else {
+            alert('The File APIs are not fully supported in this browser.');
+        }
+        /**
+         * Handle Single-file Select
+         */
         var selectedFile = null;
-        handleSingleFile = function () {
-            selectedFile = $('#input')[0].files[0]; // Could also be var selectedFile = $('#input').get(0).files[0]; 
+        handleSingleFile = function (event) {
+            console.log('Single: ', event.target.files)
+            selectedFile = $('#inputFile')[0].files[0]; // Could also be var selectedFile = $('#input').get(0).files[0]; 
             console.log('Selected File:', selectedFile);
         }
         getImageFromFile = function (fileBlob) {
             //Process File Blob
         }
-        handleFiles = function () {
-            var filesList = this.files;
-            console.log('Files List', filesList);
+        /**
+         * Handle Multiple-files select 
+         * */
+        handleFiles = function (evt, files) {
+            var filesList = files;
+            console.log('Multi: ', filesList)
+            var files = evt.target.files; // FileList object
+            console.log('Files ', files, ' from event ', evt)
+
+            // files is a FileList of File objects. List some properties.
+            var output = [];
+            for (var i = 0, f; f = files[i]; i++) {
+                output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                    f.size, ' bytes, last modified: ',
+                    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                    '</li>');
+            }
+            document.getElementById('mFiles').innerHTML = '<ul>' + output.join('') + '</ul>';
+
         }
-        //Beautified file select elements
+        /**
+         * Custom Styled file-select elements
+         */
         var fileSelect = document.getElementById("fileSelect"),
             fileElem = document.getElementById("fileElem");
 
@@ -48,8 +77,74 @@
             e.preventDefault(); // prevent navigation to "#"
         }, false);
 
-        //Forms API
+        handleFilesCustomElement = function (files) {
+            var filesList = files;
+            document.getElementById('fileNames').innerHTML = (function (fl) {
+                if (fl) {
+                    var keysList = Object.keys(fl);
+                    var kl = keysList.length;
+                    return kl > 0 ? (kl > 1 ? kl + ' files' : fl[kl - 1].name) : 'No files chosen';
+                }
+            })(filesList);
+        }
+        /**
+         * Drag and drop file-select
+         */
+        function handleDragDropFileSelect(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
 
+            var files = evt.dataTransfer.files; // FileList object.
+
+            // files is a FileList of File objects. List some properties.
+            var output = [];
+            for (var i = 0, f; f = files[i]; i++) {
+                // Only process image files.
+                if (!f.type.match('image.*')) {
+                    continue;
+                }
+
+                var reader = new FileReader();
+
+                // Closure to capture the file information.
+                reader.onload = (function (theFile) {
+                    return function (e) {
+                        // Render thumbnail.
+                        var span = document.createElement('span');
+                        span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+                        document.getElementById('list').insertBefore(span, null);
+                    };
+                })(f);
+
+                // Read in the image file as a data URL.
+                reader.readAsDataURL(f);
+
+                //Display file info in output field
+                
+                output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                    f.size, ' bytes, last modified: ',
+                    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                    '</li>');
+            }
+            document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+        }
+
+        function handleDragOver(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+        }
+
+        // Setup the dnd listeners.
+        var dropZone = document.getElementById('drop_zone');
+        dropZone.addEventListener('dragover', handleDragOver, false);
+        dropZone.addEventListener('drop', handleDragDropFileSelect, false);
+
+
+        /**
+         * Forms API
+         */
 
         //FileSystem API
 
